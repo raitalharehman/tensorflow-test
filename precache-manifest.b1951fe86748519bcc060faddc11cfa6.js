f1a79307375d7c31b8000c68739ceb2ab2ccc924
raitalharehman <talharaay@gmail.com>
2019-05-19 22:00:58 +0500

    Updates

---
+++ precache-manifest.b1951fe86748519bcc060faddc11cfa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b9f65aa17dcab46eda81d0199451e",
+    "url": "/tensorflow-test/index.html"
+  },
+  {
+    "revision": "9b0aa80de9d3eb738ed3",
+    "url": "/tensorflow-test/static/css/main.6fca2292.chunk.css"
+  },
+  {
+    "revision": "5cc3cc9b945face5022e",
+    "url": "/tensorflow-test/static/js/2.29315016.chunk.js"
+  },
+  {
+    "revision": "9b0aa80de9d3eb738ed3",
+    "url": "/tensorflow-test/static/js/main.340e8b67.chunk.js"
+  },
+  {
+    "revision": "136558aad83c5c8d6a33",
+    "url": "/tensorflow-test/static/js/runtime~main.0226b021.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tsml/precache-manifest.3d8509b2bad6226e9388f1b3612eb839.js"
+  "/tensorflow-test/precache-manifest.b1951fe86748519bcc060faddc11cfa6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tsml/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tensorflow-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
